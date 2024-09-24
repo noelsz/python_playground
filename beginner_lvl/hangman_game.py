9b76ce122d98706d4d666d5f1d98fd06763a755d
Noel Szepessy <noel.szepessy@silabs.com>
2024-09-24 11:39:09 +0200

    hangman game done

--- beginner_lvl/hangman_game.py
+++ beginner_lvl/hangman_game.py
@@ -1,10 +1,36 @@
 import nltk
 import random
 
-nltk.download('words')
+# nltk.download('words') ----> if the word list from the library hasn't been downloaded yet
 
-word_list = list(set(w.lower() for w in nltk.corpus.words.words()))
 
-word = random.choice(word_list)
+def letter_index(word, letter):
+    return [i for i, l in enumerate(word) if l == letter]
 
-print(word)
+
+def hangman_game():
+    word_list = list(set(w.lower() for w in nltk.corpus.words.words()))
+    word = random.choice(word_list)
+    guess = ''
+    wrong_guesses = 0
+    for letter in word:
+        guess += '_'
+    print(guess)
+    while guess != word and wrong_guesses <= 15:
+        letter = input('Enter a letter: ')
+        if letter in word:
+            indexes = letter_index(word, letter)
+            word_letters = list(guess)
+            for number in indexes:
+                word_letters[number] = letter
+            guess = ''.join(word_letters)
+            print(guess)
+        else:
+            wrong_guesses += 1
+
+    if guess == word:
+        print('Congratulations! You won!')
+    else:
+        print('Maybe next time...')
+
+hangman_game()
